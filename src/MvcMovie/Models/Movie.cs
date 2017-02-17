9a5806a1819b01c172fb91ac5cad576c703ce4d3
dev0 <dev0>
2017-02-17 13:33:43 -0500

    adding a model step

--- src/MvcMovie/Models/Movie.cs
+++ src/MvcMovie/Models/Movie.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models
 {
@@ -9,6 +7,7 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
